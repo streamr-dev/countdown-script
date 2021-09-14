15a8082d481aef540a0d281a5349b1421a35ae3e
juslesan <santeri.juslenius@gmail.com>
2021-09-14 14:27:47 +0300

    Update to testnet2

--- countdown.js
+++ countdown.js
@@ -4,20 +4,19 @@ const program = require('commander')
 const prettyMilliseconds = require('pretty-ms');
 const { version: CURRENT_VERSION } = require('./package.json')
 
-
 program
     .version(CURRENT_VERSION)
-    .option('--streamId <streamId>', 'streamId to publish', 'streamr.eth/brubeck-testnet/rewards')
-    .option('--eventName <eventName>', 'Which event the script is counting down to', 'BRUBECK TESTNET 1 LAUNCH')
-    .option('--preEventMessage <preEventMessage>', 'Additional message before event has started', 'If you see this message, your node has successfully subscribed to the rewards stream. You may see some reward codes being claimed already before the testnet launches. They are only there for testing and have no value.')
+    .option('--streamIds <streamIds>', 'streamIds to publish to',  (value) => value.split(','), ['streamr.eth/brubeck-testnet/rewards/5hhb49', 'streamr.eth/brubeck-testnet/rewards/95hc37', 'streamr.eth/brubeck-testnet/rewards/12ab22', 'streamr.eth/brubeck-testnet/rewards/z15g13', 'streamr.eth/brubeck-testnet/rewards/111249', 'streamr.eth/brubeck-testnet/rewards/0g2jha', 'streamr.eth/brubeck-testnet/rewards/fijka2', 'streamr.eth/brubeck-testnet/rewards/91ab49', 'streamr.eth/brubeck-testnet/rewards/giab22', 'streamr.eth/brubeck-testnet/rewards/25kpf4'])
+    .option('--eventName <eventName>', 'Which event the script is counting down to', 'BRUBECK TESTNET 2 LAUNCH')
+    .option('--preEventMessage <preEventMessage>', 'Additional message before event has started', 'If you see this message, your node has successfully subscribed to a rewards stream. You may see some reward codes being claimed already before the testnet launches. They are only there for testing and have no value.')
     .option('--postEventMessage <postEventMessage>', 'Additional message once event has started', 'Network incentives are now active, and your node should start claiming the first rewards within 10 minutes.')
     .option('--eventMessageCount <eventMessageCount>', 'Number of times to send the message after the event has started', '1')
     .option('--notificationInterval <notificationInterval>', 'interval to publish notifications in ms', '60000')
-    .option('--eventTime <eventTime>', 'Timestamp to countdown to as Unix timestamp', '1630411200')
+    .option('--eventTime <eventTime>', 'Timestamp to countdown to as Unix timestamp', '1631793600')
     .description('Run Countdown script')
     .parse(process.argv)
 
-const streamId = program.opts().streamId
+const streamIds = program.opts().streamIds
 const eventName = program.opts().eventName
 const preEventMessage = program.opts().preEventMessage
 const postEventMessage = program.opts().postEventMessage
@@ -54,7 +53,10 @@ const start = () => {
         }
         console.log(msg)
         await client.connect()
-        await client.publish(streamId, msg)
+        const promises = streamIds.map(async (streamId) => {
+            await client.publish(streamId, msg)
+        })
+        await Promise.all(promises)
 
         if (eventMessageCount < eventMessageCounter) {
             clearInterval(publishIntervalRef)
