f617ef1179356516bc4990e3cfa96d675934930b
juslesan <santeri.juslenius@gmail.com>
2021-10-07 11:40:09 +0300

    fix messages

--- countdown.js
+++ countdown.js
@@ -10,7 +10,7 @@ const TRACKER_REGISTRY_PATH = 'trackerRegistry.json'
 program
     .version(CURRENT_VERSION)
     .option('--streamIds <streamIds>', 'streamIds to publish to',  (value) => value.split(','), ['streamr.eth/brubeck-testnet/rewards/5hhb49', 'streamr.eth/brubeck-testnet/rewards/95hc37', 'streamr.eth/brubeck-testnet/rewards/12ab22', 'streamr.eth/brubeck-testnet/rewards/z15g13', 'streamr.eth/brubeck-testnet/rewards/111249', 'streamr.eth/brubeck-testnet/rewards/0g2jha', 'streamr.eth/brubeck-testnet/rewards/fijka2', 'streamr.eth/brubeck-testnet/rewards/91ab49', 'streamr.eth/brubeck-testnet/rewards/giab22', 'streamr.eth/brubeck-testnet/rewards/25kpf4'])
-    .option('--eventName <eventName>', 'Which event the script is counting down to', 'BRUBECK TESTNET 3 LAUNCH (Brubeck Client Test)')
+    .option('--eventName <eventName>', 'Which event the script is counting down to', 'BRUBECK TESTNET 3 LAUNCH')
     .option('--preEventMessage <preEventMessage>', 'Additional message before event has started', 'If you see this message, your node has successfully subscribed to a rewards stream. You may see some reward codes being claimed already before the testnet launches. They are only there for testing and have no value.')
     .option('--postEventMessage <postEventMessage>', 'Additional message once event has started', 'Network incentives are now active, and your node should start claiming the first rewards within 10 minutes.')
     .option('--eventMessageCount <eventMessageCount>', 'Number of times to send the message after the event has started', '1')
